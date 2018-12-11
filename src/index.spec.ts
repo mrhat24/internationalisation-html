3c421951416f1a0796be452cb1f9e4a1d3877add
mrha24 <victimlisten@gmail.com>
2018-12-11 14:07:15 +0700

    version 1.1.0

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,7 +7,7 @@ describe("test translator functions", function () {
         const replace = 'pogChamp';
         const index = 0;
         const result = replaceAt(str, substr, replace, index);
-       expect(result).toEqual('pogChamp, !#test#! and !#test#! has come to the city.');
+        expect(result).toEqual('pogChamp, !#test#! and !#test#! has come to the city.');
     });
 
     it("test translate function", function () {
@@ -32,7 +32,7 @@ describe("test translator functions", function () {
         let t = new Translator(parent, 'ru', {ru: {test: 'ru translate'}, en: {test: 'eng translate'}});
         expect(parent.querySelector('#child').getAttribute('alt')).toEqual('ru translate');
         setTimeout(() => {
-            t.init('en');
+            t.translateNodes('en');
         }, 100);
         setTimeout(() => {
             expect(parent.querySelector('#child').getAttribute('alt')).not.toEqual('ru translate');
@@ -40,5 +40,45 @@ describe("test translator functions", function () {
             done();
         }, 1000);
     });
+
+    it('translateAttributes function', function () {
+       const element = document.createElement('div');
+       element.setAttribute('alt', 'text');
+       element.setAttribute('title', '!#test#!');
+       Translator.translateAttributes(element.attributes,  {test: 'test 228'}, /!#(\w+)#!/);
+       expect(element.getAttribute('title')).toEqual('test 228');
+    });
+
+    it('handle child nodes function', function () {
+       const element = document.createElement('div');
+        element.innerHTML = `<div id="child" alt="!#test#!">!#test2#!</div>`;
+        Translator.handleChildNodes(element,  {test: 'test 228', test2: 'test 229'}, /!#(\w+)#!/);
+        expect(element.querySelector('#child').getAttribute('alt')).toEqual('test 228');
+        expect(element.querySelector('#child').textContent).toEqual('test 229');
+    });
+
+    it('getAttrsForTranslate testing', function () {
+        const element = document.createElement('div');
+        element.setAttribute('alt', 'text');
+        element.setAttribute('title', '!#test#!, !#test#!, !#dfafads#!');
+        let result = Translator.getAttrsForTranslate(element,  /!#(\w+)#!/);
+        expect(result.length).toEqual(3);
+    });
+
+    it('getTextForTranslate testing', function () {
+        const element = document.createElement('div');
+        element.innerHTML = "!#test#!, !#test2#!";
+        let result = Translator.getTextForTranslate(element,  /!#(\w+)#!/);
+        expect(result.length).toEqual(2);
+    });
+
+
+    it('testing child nodes algorytm', function () {
+        const parent = document.createElement('div');
+        parent.innerHTML = `<div alt="!#test#!">!#test 228#!</div>`;
+        let t = new Translator(parent, 'ru', {ru: {test: 'test 228'}});
+        expect(t.translatedNodes.length).not.toEqual(0);
+    });
+
 
 });
--- src/index.ts
+++ src/index.ts
@@ -77,16 +77,20 @@ export class Translator {
     private _defaultElementsState: {item: HTMLElement, text: string}[] = [];
     private _defaultAttributeState: {item: HTMLElement, attributeName: string, value: string, key: string}[] = [];
 
+    public translatedNodes: {node: HTMLElement, defaultValue: string, index: number, type: string, attributeName?: string, key: string}[] = [];
+
     constructor(
         public element: HTMLElement,
         public language: string,
         public dictionaries: Dictionaries,
         public defaultRegex: RegExp = /!#(\w+)#!/
     ) {
-        this._translationElements = element.querySelectorAll<HTMLElement>('[translate]');
-        this._translationAttributes = element.querySelectorAll<HTMLElement>('[translateattribute]');
-        this.setArrays();
-        this.init(this.language);
+        // this._translationElements = element.querySelectorAll<HTMLElement>('[translate]');
+        // this._translationAttributes = element.querySelectorAll<HTMLElement>('[translateattribute]');
+        // this.setArrays();
+        // this.init(this.language);
+        this.setTranslatedNodes(this.element);
+        this.translateNodes(this.language);
     }
 
     private setArrays() {
@@ -123,6 +127,141 @@ export class Translator {
         }
     }
 
+    public initNew(lang) {
+        if (!this.dictionaries[lang]) {
+            return;
+        }
+        this.reset();
+        Translator.handleChildNodes(this.element, this.dictionaries[lang], this.defaultRegex);
+        // for (let i = 0; i < this._defaultElementsState.length; i++) {
+        //     let translate = Translator.translate(this._translationElements[i].innerText, this.dictionaries[lang], this.defaultRegex);
+        // }
+        // for (let i = 0; i < this._defaultAttributeState.length; i++) {
+        //     let attr = this._defaultAttributeState[i].attributeName;
+        //     let trans = Translator.translate(this._defaultAttributeState[i].value, this.dictionaries[lang], this.defaultRegex);
+        //     this._defaultAttributeState[i].item.setAttribute(attr, trans);
+        // }
+    }
+
+
+    public static handleChildNodes(node: Node, dictionary: Dictionary, regex: RegExp) {
+        node.childNodes.forEach((value: HTMLInputElement) => {
+            if (value.nodeType === 1) {
+                Translator.handleChildNodes(value, dictionary, regex);
+                Translator.translateAttributes(value.attributes, dictionary, regex);
+            } else if (value.nodeType === 3) {
+                Translator.translateText(value, dictionary, regex);
+            }
+        });
+    }
+
+    public translateNodes(lang) {
+        this.resetNodes();
+        let dictionary = this.dictionaries[lang];
+        this.translatedNodes.map(item => {
+            if (item.type == 'attribute') {
+                item.node.setAttribute(item.attributeName, replaceAt(item.node.getAttribute(item.attributeName), item.defaultValue, dictionary[item.key], item.index));
+            } else if (item.type == 'text') {
+                item.node.nodeValue = replaceAt(item.node.nodeValue, item.defaultValue, dictionary[item.key], item.index);
+            }
+        });
+    }
+
+    public resetNodes() {
+        this.translatedNodes.map(item => {
+            if (item.type == 'attribute') {
+                item.node.setAttribute(item.attributeName, item.defaultValue);
+            } else if (item.type == 'text') {
+                item.node.nodeValue = item.defaultValue;
+            }
+        })
+    }
+
+    public setTranslatedNodes(parent: HTMLElement) {
+        console.log('parent', parent);
+        parent.childNodes.forEach((value: any) => {
+            console.log('value', value.nodeValue);
+            if (value.nodeType === 1) {
+                console.log(value.attributes);
+                let a = value.nodeType;
+                let names = value.getAttributeNames();
+
+                let attrs = Translator.getAttrsForTranslate(value, this.defaultRegex);
+
+                for (let item of attrs) {
+                    this.translatedNodes.push({
+                        ...item,
+                        type: 'attribute',
+                        node: value
+                    });
+                }
+                this.setTranslatedNodes(value);
+            } else if (value.nodeType === 3) {
+                console.log(value.nodeValue);
+                let texts = Translator.getTextForTranslate(value, this.defaultRegex);
+                console.log('texts', texts);
+                for (let item of texts) {
+                    this.translatedNodes.push({
+                        ...item,
+                        type: 'text',
+                        node: value
+                    });
+                    console.log('push item', value);
+                }
+            }
+        });
+    }
+
+    public static getTextForTranslate(element: Node, regex: RegExp): {defaultValue: string, index: number, key: string}[] {
+        let result = [];
+        let newRegex = new RegExp(regex, 'g');
+        let rr;
+        let text = element.nodeValue;
+        console.log(element);
+        while ((rr = newRegex.exec(text)) !== null) {
+            result.push({
+                defaultValue: rr[0],
+                index: rr.index,
+                key: rr[1]
+            });
+        }
+        return result;
+    }
+
+    public static getAttrsForTranslate(element: HTMLElement, regex: RegExp): {defaultValue: string, index: number, attributeName: string, key: string}[] {
+        let result = [];
+        let keys = element.getAttributeNames();
+        let i = 0;
+        let newRegex = new RegExp(regex, 'g');
+        for (let key of keys) {
+            let rr;
+            let attrValue = element.getAttribute(key);
+            while ((rr = newRegex.exec(attrValue)) !== null) {
+                result.push({
+                    defaultValue: rr[0],
+                    index: rr.index,
+                    attributeName: key,
+                    key: rr[1]
+                });
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public static translateText(node: Node, dictionary: Dictionary, regex: RegExp) {
+        node.nodeValue = Translator.translate(node.nodeValue, dictionary, regex);
+        console.log('new text value', node.nodeValue);
+    }
+
+    public static translateAttributes(attributes: NamedNodeMap, dictionary: Dictionary, regex: RegExp) {
+        let keys = Object.keys(attributes);
+        for (let key of keys) {
+            attributes[key].value = Translator.translate(attributes[key].value, dictionary, regex);
+            console.log('new attribute value', attributes[key].value);
+        }
+    }
+
     public reset() {
         for (let i = 0; i < this._defaultElementsState.length; i++) {
             this._defaultElementsState[i].item.innerText = this._defaultElementsState[i].text;
@@ -132,11 +271,6 @@ export class Translator {
             let trans = this._defaultAttributeState[i].value;
             this._defaultAttributeState[i].item.setAttribute(attr, trans);
         }
-    }
-
-    public static translateAttribute(text: string, dictionary: Dictionary) {
-        let regex = /!#(\w+)#!/;
-        return dictionary[text];
     }
 
     public static translate(text: string, dictionary: Dictionary, regex: RegExp): string {
